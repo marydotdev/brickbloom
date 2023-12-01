8dee9955d315e79a956368c3e6c9a233db3738b1
marydotdev <mary.haedrich@gmail.com>
2023-12-01 13:05:10 -0500

    supabase

--- next.config.js
+++ next.config.js
@@ -1,31 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "replicate.com",
-      },
-      {
-        protocol: "https",
-        hostname: "replicate.delivery",
-      },
-      {
-        protocol: "https",
-        hostname: "*.replicate.delivery",
-      },
-      {
-        protocol: "https",
-        hostname: "oaidalleapiprodscus.blob.core.windows.net",
-      },
-      {
-        protocol: "https",
-        hostname: "ennwjiitmiqwdrgxkevm.supabase.co",
-      },
-    ],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
