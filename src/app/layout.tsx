7b95f274c013402a3e1399ba2661921db89e9ab7
marydotdev <mary.haedrich@gmail.com>
2023-12-08 17:15:21 -0500

    basics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,10 @@ import Link from "next/link";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "sonner";
+import { AFFILIATE_LINKS } from '@/lib/constants';
+
+const adList = AFFILIATE_LINKS.sort(() => Math.random() - 0.5);
+const ads = adList.slice(0, 3);
 
 
 const inter = Inter({
@@ -44,12 +48,43 @@ export default function RootLayout({
             </Link>
           </div>
         </div>
+
         <main className="flex w-full flex-col items-center pb-12">
           {children}
         </main>
+        <div className="w-full max-w-6xl mx-auto border border-black flex justify-between">
+          {ads.map((link) => (
+            <div
+              className="relative border-2 border-red-600 cursor-pointer"
+              key={link.title}
+            >
+              <a href={link.url} target="_blank" rel="noreferrer" className='flex flex-col'>
+                <div className="w-64 h-64">
+                  <Image
+                    src={link.image}
+                    alt={link.title}
+                    width={256}
+                    height={256}
+                  />
+                </div>
+                <div className='p-4'>
+                  <p className="text-center">{link.title}</p>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
         <footer className="w-full py-4 lg:py-12">
           <p className="text-center text-zinc-600">
-            made by <a href="https://twitter.com/marydotdev" target='_blank' rel="noreferrer" className='hover:text-sky-500'>mary</a>{" "}
+            made by{" "}
+            <a
+              href="https://twitter.com/marydotdev"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:text-sky-500"
+            >
+              mary
+            </a>{" "}
           </p>
         </footer>
         <Analytics />
