5039e4575ee6ccabe7f7cfaaf54a73415053d324
marydotdev <mary.haedrich@gmail.com>
2024-02-08 14:28:50 -0500

    trying config again

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "replicate.com",
+        "*.replicate.com",
+        "api.replicate.com",
+        "replicate.delivery",
+      ],
+    },
+  },
   images: {
     domains: [
       "a6ar4ev9eff2kzqs.public.blob.vercel-storage.com",
