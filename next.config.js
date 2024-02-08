1a08ea7e962fe863a642d0258acb49f21955bebe
marydotdev <mary.haedrich@gmail.com>
2024-02-08 13:26:29 -0500

    add config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: [
+      "a6ar4ev9eff2kzqs.public.blob.vercel-storage.com",
+      "replicate.delivery",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
