bf191cabf1f3fe851dca5bef073318ec05ad7b01
marydotdev <mary.haedrich@gmail.com>
2024-02-21 20:36:34 -0500

    add delete to gallery

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -3,6 +3,18 @@
 import { useEffect, useState } from "react";
 import { getUserId } from "@/lib/utils";
 import { ImageCard } from "@/components/ImageCard";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
 
 type Image = {
   image_url: string;
@@ -26,11 +38,46 @@ const Gallery = () => {
       {images.map((image, index) => (
         <div
           key={index}
-          className="max-w-md px-2 py-4 flex flex-col gap-4 bg-white lg:hover:transform  lg:hover:scale-105 transition-all duration-100 ease-in-out"
+          className="relative max-w-md px-2 py-4 flex flex-col gap-4 bg-white lg:hover:transform  lg:hover:scale-105 transition-all duration-100 ease-in-out"
         >
           <a href={`/${image.id}`}>
-          <ImageCard imageURL={image.image_url} prompt={image.prompt} id={image.id} />
+            <ImageCard
+              imageURL={image.image_url}
+              prompt={image.prompt}
+              id={image.id}
+            />
           </a>
+          <div className='absolute bottom-4'>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="ghost" size={"icon"}>
+                  <span className="sr-only">Delete</span>
+                  <Trash className="h-4 w-4" />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                  <DialogTitle>Are you sure?</DialogTitle>
+                  <DialogDescription className='pt-2'>
+                    This action cannot be undone. Are you sure you want to
+                    delete this image forever?
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className="">
+                  <DialogClose asChild>
+                    <div className="flex gap-2">
+                      <Button type="button" variant="destructive">
+                        Delete
+                      </Button>
+                      <Button type="button" variant="outline">
+                        Cancel
+                      </Button>
+                    </div>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
         </div>
       ))}
     </div>
