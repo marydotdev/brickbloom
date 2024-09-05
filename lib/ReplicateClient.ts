c5f58bd6abaeecca6b62916028b6df30ae34c143
marydotdev <mary.haedrich@gmail.com>
2024-09-05 16:03:39 -0400

    trying flux model

--- lib/ReplicateClient.ts
+++ lib/ReplicateClient.ts
@@ -1,4 +1,3 @@
-
 import Replicate from "replicate";
 import { ReplicateRequest, ReplicateResponse } from "./types";
 
@@ -14,13 +13,58 @@ export class ReplicateClient {
   /**
    * Generate image
    */
+  // generateImage = async (request: ReplicateRequest): Promise<string> => {
+  //   const output = (await this.replicate.run(
+  //     "marydotdev/sdxl-lego:9ddc2c9883e658f1317fd39b4d150ff79376cc8e63421a97ab5d0d5d757e1ab6",
+  //     {
+  //       input: {
+  //         prompt: request.prompt,
+  //         negative_prompt: request.negative_prompt,
+  //       },
+  //     }
+  //   )) as ReplicateResponse;
+
+  //   if (!output) {
+  //     throw new Error("Failed to generate QR code");
+  //   }
+
+  //   return output[0];
+  // };
+
+  // generateImage = async (request: ReplicateRequest): Promise<string> => {
+  //   const output = (await this.replicate.run(
+  //     "marydotdev/flex-lego:7a2b0fd5f1a8cdec8014820848584af41cd34476ab34ef2001481cd8ff447f92",
+  //     {
+  //       input: {
+  //         prompt: request.prompt,
+  //         negative_prompt: request.negative_prompt,
+  //         model: "dev",
+  //         lora_scale: 1,
+  //         num_outputs: 1,
+  //         aspect_ratio: "1:1",
+  //         output_format: "webp",
+  //         guidance_scale: 3.5,
+  //         output_quality: 90,
+  //         prompt_strength: 0.8,
+  //         extra_lora_scale: 1,
+  //         num_inference_steps: 28,
+  //       },
+  //     }
+  //   )) as ReplicateResponse;
+
+  //   if (!output) {
+  //     throw new Error("Failed to generate QR code");
+  //   }
+
+  //   return output[0];
+  // };
+
   generateImage = async (request: ReplicateRequest): Promise<string> => {
     const output = (await this.replicate.run(
-      "marydotdev/sdxl-lego:9ddc2c9883e658f1317fd39b4d150ff79376cc8e63421a97ab5d0d5d757e1ab6",
+      "marydotdev/brickbloom-flux:7a2b0fd5f1a8cdec8014820848584af41cd34476ab34ef2001481cd8ff447f92",
       {
         input: {
           prompt: request.prompt,
-          negative_prompt: request.negative_prompt,
         },
       }
     )) as ReplicateResponse;
